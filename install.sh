79b53e43cb642318ef5c529a4da8c77658b01b05
Safin Singh <42120904+safinsingh@users.noreply.github.com>
2020-06-13 23:57:52 -0700

    require score to be run as root

--- install.sh
+++ install.sh
@@ -28,5 +28,13 @@ pip3 install -r requirements.txt
 
 echo "* * * * * root /usr/bin/python3 /opt/ParadoxSE/worker.py" > /etc/crontab
 
-echo "/usr/bin/python3 /opt/ParadoxSE/worker.py" > /usr/local/bin/score
+echo """
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+/usr/bin/python3 /opt/ParadoxSE/worker.py
+""" > /usr/local/bin/score
+
 chmod +x /usr/local/bin/score
