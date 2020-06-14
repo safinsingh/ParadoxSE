be1cb3cc4988dfecedeeaa211778091bff6b33d2
safinsingh <safin.singh@gmail.com>
2020-06-13 22:01:39 -0700

    add installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+echo """
+
+██████╗  █████╗ ██████╗  █████╗ ██████╗  ██████╗ ██╗  ██╗███████╗███████╗
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔═══██╗╚██╗██╔╝██╔════╝██╔════╝
+██████╔╝███████║██████╔╝███████║██║  ██║██║   ██║ ╚███╔╝ ███████╗█████╗  
+██╔═══╝ ██╔══██║██╔══██╗██╔══██║██║  ██║██║   ██║ ██╔██╗ ╚════██║██╔══╝  
+██║     ██║  ██║██║  ██║██║  ██║██████╔╝╚██████╔╝██╔╝ ██╗███████║███████╗
+╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝
+                                                                         
+"""
+
 cd /opt
 
 apt install git
+apt install python3-pip
+
 git clone https://github.com/safinsingh/ParadoxSE.git
 cd ParadoxSE
+
+pip3 install -r requirements.txt
+
+echo "* * * * * root /usr/bin/python3 /opt/ParadoxSE/worker.py" > /etc/crontab
+
+echo "/usr/bin/python3 /opt/ParadoxSE/worker.py" > /usr/local/bin/score
+chmod +x /usr/local/bin/score
