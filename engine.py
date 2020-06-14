f9f0bb999824e836de357ea7374b3705b07d369f
safinsingh <safin.singh@gmail.com>
2020-06-13 20:51:22 -0700

    add succesful user_in_group

--- engine.py
+++ engine.py
@@ -61,6 +61,11 @@ class ParadoxSE():
         if obj[1]["group"] not in [entry.gr_name for entry in grp.getgrall()]:
             return obj[0]["name"], obj[2]["points"]
 
+    def user_in_group(self, obj):
+        if self.group_exists([{"name": ""}, {"group": obj[2]["group"]}, {"points": 0}]) != None:
+            if obj[1]["user"] in grp.getgrnam(obj[2]["group"]).gr_mem:
+                return obj[0]["name"], obj[3]["points"]
+
     def update(self):
         self.apt = apt.Cache()
 
